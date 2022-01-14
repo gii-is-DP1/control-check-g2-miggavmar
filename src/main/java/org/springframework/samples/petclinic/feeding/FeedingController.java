1c31e9bc9f7f1de163c011db2734e82c5a3b55f4
Miguel <miggavmar@alum.us.es>
2022-01-14 12:20:17 +0100

    me funcionan todos los tests, excepto el test 8 y test 10

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,5 +1,52 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.exceptions.DuplicatedPetNameException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feeding")
 public class FeedingController {
     
+	@Autowired
+	private FeedingService feedingService;
+	
+	private static final String VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM = "feedings/createOrUpdateFeedingForm";
+	
+	@GetMapping(path = "/create")
+	public String initCreationForm(ModelMap model) {
+		model.addAttribute("feeding", new Feeding());
+		model.addAttribute("feedingType", feedingService.getAllFeedingTypes());
+		return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(path = "/create")
+	public String processCreationForm(@Valid Feeding feeding, BindingResult result, ModelMap model) {
+		String view = "welcome";
+		if (result.hasErrors()) {
+			model.addAttribute("feeding", feeding);
+			model.addAttribute("feedingType", feedingService.getAllFeedingTypes());
+			return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+		}else {
+			try{
+				this.feedingService.save(feeding);
+            }catch(UnfeasibleFeedingException ex){
+               result.rejectValue("pet", "La mascota seleccionada no se le puede asignar el plan de alimentación especificado.");
+                return VIEWS_FEEDINGS_CREATE_OR_UPDATE_FORM;
+            }
+			model.addAttribute("message", "¡Se ha guardado correctamente!");
+			
+		}
+		return view;	
+			
+		
+	}
+	
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -1,22 +1,45 @@
 package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.exceptions.DuplicatedPetNameException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class FeedingService {
+	
+	@Autowired
+	private FeedingRepository feedingRepository;
+	
     public List<Feeding> getAll(){
-        return null;
+    	List<Feeding> f = feedingRepository.findAll();
+        return f;
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+    	List<FeedingType> f = feedingRepository.findAllFeedingTypes();
+        return f;
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+    	FeedingType fT = feedingRepository.findFeedingType(typeName);
+        return fT;
     }
 
+    
+    @Transactional(rollbackFor = UnfeasibleFeedingException.class)
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+    	Optional<Feeding>  f = feedingRepository.findById(p.getId());
+    	if(p.getPet().getType()!= f.get().getPet().getType()) {
+    		throw new UnfeasibleFeedingException();
+    	}else {
+    		Feeding res = feedingRepository.save(p);
+    		return res; 
+    	}
+              
     }
 
     
